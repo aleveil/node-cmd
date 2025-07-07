ea28e26b91f44ba2b6d7be362d6ed5dc5db0f1d7
aleveil <anthony.leveil@protonmail.com>
2025-07-07 21:26:13 +0200

    fix ls and cd

--- cmd_bin/cd.js
+++ cmd_bin/cd.js
@@ -1,17 +1,24 @@
 import fs from "fs"
 import path from "path"
 
-export default function(args, pwd, cmdEnvPath) {
+export default function (args, pwd, cmdEnvPath) {
     if (args.length !== 1) {
         return {
             output: "Bad args: cd path"
         }
     }
-    let target = args[0].toLowerCase()
-    // Calculate new pwd path like this to prevent getting out of cmd_env
-    let newPwd = path.join(pwd, target)
-    let truePath = path.join(cmdEnvPath, newPwd)
 
+    let isAbsolute = false
+    let target = "."
+    if (!!args[0]) {
+        target = args[0].toLowerCase()
+        isAbsolute = target.charAt(0) === "/"
+    }
+
+    // Calculate new pwd path like this to prevent getting out of cmd_env
+    let targetPath = isAbsolute ? path.join(target.charAt(0), target.slice(1)) : path.join(pwd, target)
+    let truePath = path.join(cmdEnvPath, targetPath)
+    
     if (!fs.existsSync(truePath)) {
         return {
             output: `Error: ${target} does not exist`
@@ -23,6 +30,6 @@ export default function(args, pwd, cmdEnvPath) {
         }
     }
     return {
-        newPwd
+        newPwd: targetPath
     }
 }
--- cmd_bin/ls.js
+++ cmd_bin/ls.js
@@ -7,7 +7,29 @@ export default function (args, pwd, cmdEnvPath) {
             output: "Bad args: ls [path]"
         }
     }
-    let truePath = path.join(cmdEnvPath, pwd)
+
+    let isAbsolute = false
+    let target = "."
+    if (!!args[0]) {
+        target = args[0].toLowerCase()
+        isAbsolute = target.charAt(0) === "/"
+    } 
+
+    // Calculate new pwd path like this to prevent getting out of cmd_env
+    let targetPath = isAbsolute ? path.join(target.charAt(0), target.slice(1)) : path.join(pwd, target)
+    let truePath = path.join(cmdEnvPath, targetPath)
+
+    if (!fs.existsSync(truePath)) {
+        return {
+            output: `Error: ${args[0]} does not exist`
+        }
+    }
+    if (!fs.lstatSync(truePath).isDirectory()) {
+        return {
+            output: `Error: ${args[0]} is not a directory`
+        }
+    }
+
     let list = fs.readdirSync(truePath);
 
     return {
